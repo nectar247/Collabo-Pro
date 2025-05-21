daf95716f45c022032cc5e2ec95a1a0d412af0df
Dickson <olejemedickson@gmail.com>
2025-05-21 19:16:29 +0100

    Admin fix3

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -2,12 +2,16 @@ import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
 import AdminDashboardClient from './components/AdminDashboardClient'
 
-export const dynamic = 'force-dynamic'; // optional, if this page uses runtime data
-export const fetchCache = 'force-no-store'; // optional, disables caching
 
-export default async function AdminPage() {
+export const dynamic = 'force-dynamic';
+export const fetchCache = 'force-no-store';
+
+export default function AdminPageWrapper() {
+  return <AdminPage />;
+}
+
+async function AdminPage() {
   const token = cookies().get('authToken')?.value;
-  console.log('Admin token on server:', token);
 
   if (!token) {
     redirect('/sign-in');
